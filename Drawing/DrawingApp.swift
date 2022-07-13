fcbeee16c6a0bcad9ada3197b3580ba9e37a7584
derya <derya@boonandmoil.dev>
2022-07-13 08:54:28 +0100

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Derya Antonelli on 13/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
